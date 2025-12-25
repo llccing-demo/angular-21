b202b0049f82ae39dcc3a2792afb515e636aabb3
Rowan <lcf33123@gmail.com>
2025-12-25 16:53:56 +0800

    feat: Reacting to signal changes with effects

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, computed, effect } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { Hightlight } from './directives/hightlight';
 
@@ -11,4 +11,47 @@ import { Hightlight } from './directives/hightlight';
 })
 export class App {
   protected readonly title = signal('angular-21');
+
+  theme = signal<'light' | 'dark'>('light');
+  username = signal('Guest');
+  isLoginIn = signal(false);
+
+  themeClass = computed(() => this.theme() === 'light' ? 'theme-light' : 'theme-dark');
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem('theme', this.theme());
+      console.log('current theme:', this.theme());
+    });
+
+    effect(() => {
+      console.log("isLoginIn changed:", this.isLoginIn());
+      console.log("username is:", this.username());
+    });
+
+    effect((onCleanup) => {
+      const interval = setInterval(() => {
+        console.log('Interval tick - Current theme', this.theme());
+      }, 5000)
+
+      onCleanup(() => {
+        clearInterval(interval)
+        console.log('Cleaned up interval');
+      });
+    });
+  }
+
+  toggleTheme() {
+    this.theme.set(this.theme() === 'light' ? 'dark' : 'light');
+  }
+
+  login() {
+    this.isLoginIn.set(true);
+    this.username.set('Rowan');
+  }
+
+  logout() {
+    this.isLoginIn.set(false);
+    this.username.set('Guest');
+  }
 }
